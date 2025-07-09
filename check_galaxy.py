f2f9e605d60ee3583a4bb9058e576b5a007f9f29
Benedikt Holland <benedikt.holland@protonmail.com>
2025-07-09 18:01:44 +0200

    fix timestamp file does not exist bug

--- check_galaxy.py
+++ check_galaxy.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import argparse
 from discord import SyncWebhook
 from dotenv import load_dotenv
-import os
 from dateutil.tz import tzlocal
 
 
@@ -17,8 +16,7 @@ if __name__ == '__main__':
     parser.add_argument("-s", "--system", default=None, type=int)
     parser.add_argument("-p", "--position", default=None, type=int)
     parser.add_argument("-n", "--galaxy-num", default=8, type=int)
-    parser.add_argument("-de", "--debug", action='store_true', default=False)
-    parser.add_argument("-w", "--webhook", type=str, required=True)
+    parser.add_argument("-w", "--webhook", default=None, type=str)
     parser.add_argument("-t", "--tag", type=str)
     args = parser.parse_args()
     url = f"https://s{args.universe}-{args.domain}.ogame.gameforge.com/api/universe.xml"
@@ -30,31 +28,30 @@ if __name__ == '__main__':
     root = tree.getroot()
     timestamp = datetime.fromtimestamp(int(root.get('timestamp'))).astimezone(tzlocal()).strftime('%d.%m.%Y %H:%M:%S Uhr')
     timestamp_filename = f'timestamp{args.universe}.txt'
-    with open(timestamp_filename, 'rt') as file:
+    with open(timestamp_filename, 'w+') as file:
         old_timestamp = file.read()
-    if args.debug or timestamp != old_timestamp:
-        with open(timestamp_filename, 'wt') as file:
+        if not args.webhook or timestamp != old_timestamp:
             file.write(timestamp)
-        open_coords = []
-        for galaxy in range(1, args.galaxy_num + 1):
-            for system in range(1, 500):
-                for position in range(1, 16):
-                    if (args.galaxy is None or args.galaxy == galaxy) and (args.system is None or args.system == system) and (args.position is None or args.position == position):
-                        open_coords.append(f"{galaxy}:{system}:{position}")
-        for planet in root.findall('planet'):
-            coords = planet.get("coords")
-            coord_list = coords.split(":")
-            if (args.galaxy is None or int(coord_list[-3]) == args.galaxy) and (args.system is None or int(coord_list[-2]) == args.system) and (args.position is None or int(coord_list[-1]) == args.position):
-                open_coords.remove(coords)
-        webhook = SyncWebhook.from_url(args.webhook)
-        if len(open_coords) > 0 and args.tag is not None:
-            message = args.tag + "\n"
-        else:
-            message = "Keine "
-        message += f"Freie 8er {timestamp}"
-        for coord in open_coords:
-            message += f"\n- {coord}"
-        if args.debug:
-            print(message)
-        else:
-            webhook.send(message)
+            open_coords = []
+            for galaxy in range(1, args.galaxy_num + 1):
+                for system in range(1, 500):
+                    for position in range(1, 16):
+                        if (args.galaxy is None or args.galaxy == galaxy) and (args.system is None or args.system == system) and (args.position is None or args.position == position):
+                            open_coords.append(f"{galaxy}:{system}:{position}")
+            for planet in root.findall('planet'):
+                coords = planet.get("coords")
+                coord_list = coords.split(":")
+                if (args.galaxy is None or int(coord_list[-3]) == args.galaxy) and (args.system is None or int(coord_list[-2]) == args.system) and (args.position is None or int(coord_list[-1]) == args.position):
+                    open_coords.remove(coords)
+            if len(open_coords) > 0 and args.tag is not None:
+                message = args.tag + "\n"
+            else:
+                message = "Keine "
+            message += f"Freie 8er {timestamp}"
+            for coord in open_coords:
+                message += f"\n- {coord}"
+            if args.webhook:
+                webhook = SyncWebhook.from_url(args.webhook)
+                webhook.send(message)
+            else:
+                print(message)
