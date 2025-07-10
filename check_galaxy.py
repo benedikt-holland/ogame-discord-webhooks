32df00d3833b751e3120dfdb3c80fd0afc7f8363
Benedikt Holland <benedikt.holland@protonmail.com>
2025-07-10 16:11:01 +0200

    remove num-galaxy command line argument

--- check_galaxy.py
+++ check_galaxy.py
@@ -13,13 +13,12 @@ if __name__ == '__main__':
     parser.add_argument("-g", "--galaxy", default=None, type=int)
     parser.add_argument("-s", "--system", default=None, type=int)
     parser.add_argument("-p", "--position", default=None, type=int)
-    parser.add_argument("-n", "--galaxy-num", default=8, type=int)
     parser.add_argument("-w", "--webhook", default=None, type=str)
     parser.add_argument("-t", "--tag", type=str)
     args = parser.parse_args()
     url = f"https://s{args.universe}-{args.domain}.ogame.gameforge.com/api/universe.xml"
     response = requests.get(url)
-    filename = f'universe.xml'
+    filename = 'universe.xml'
     with open(filename, 'wb') as file:
         file.write(response.content)
     tree = ET.parse(filename)
@@ -31,14 +30,17 @@ if __name__ == '__main__':
         if not args.webhook or timestamp != old_timestamp:
             file.write(timestamp)
             open_coords = []
-            for galaxy in range(1, args.galaxy_num + 1):
-                for system in range(1, 500):
-                    for position in range(1, 16):
-                        if (args.galaxy is None or args.galaxy == galaxy) and (args.system is None or args.system == system) and (args.position is None or args.position == position):
-                            open_coords.append(f"{galaxy}:{system}:{position}")
+            galaxies = []
             for planet in root.findall('planet'):
                 coords = planet.get("coords")
                 coord_list = coords.split(":")
+                galaxy = coord_list[0]
+                if galaxy not in galaxies:
+                    galaxies.append(galaxy)
+                    for system in range(1, 500):
+                        for position in range(1, 16):
+                            if (args.galaxy is None or args.galaxy == galaxy) and (args.system is None or args.system == system) and (args.position is None or args.position == position):
+                                open_coords.append(f"{galaxy}:{system}:{position}")
                 if (args.galaxy is None or int(coord_list[-3]) == args.galaxy) and (args.system is None or int(coord_list[-2]) == args.system) and (args.position is None or int(coord_list[-1]) == args.position):
                     open_coords.remove(coords)
             if len(open_coords) > 0 and args.tag is not None:
