33ec5b3b1ecff485d4dd2fcf7ab5f8cc5cb658d3
Benedikt Holland <benedikt.holland@protonmail.com>
2025-09-04 18:04:30 +0200

    fix write timestamp issue

--- check_galaxy.py
+++ check_galaxy.py
@@ -25,33 +25,34 @@ if __name__ == '__main__':
     root = tree.getroot()
     timestamp = datetime.fromtimestamp(int(root.get('timestamp'))).astimezone(tzlocal()).strftime('%d.%m.%Y %H:%M:%S Uhr')
     timestamp_filename = f'timestamp{args.universe}.txt'
-    with open(timestamp_filename, 'r+') as file:
+    with open(timestamp_filename, 'r') as file:
         old_timestamp = file.read()
-        if not args.webhook or timestamp != old_timestamp:
+    if not args.webhook or timestamp != old_timestamp:
+        with open(timestamp_filename, "w"):
             file.write(timestamp)
-            open_coords = []
-            galaxies = []
-            for planet in root.findall('planet'):
-                coords = planet.get("coords")
-                coord_list = coords.split(":")
-                galaxy = coord_list[0]
-                if galaxy not in galaxies:
-                    galaxies.append(galaxy)
-                    for system in range(1, 500):
-                        for position in range(1, 16):
-                            if (args.galaxy is None or args.galaxy == galaxy) and (args.system is None or args.system == system) and (args.position is None or args.position == position):
-                                open_coords.append(f"{galaxy}:{system}:{position}")
-                if (args.galaxy is None or int(coord_list[-3]) == args.galaxy) and (args.system is None or int(coord_list[-2]) == args.system) and (args.position is None or int(coord_list[-1]) == args.position):
-                    open_coords.remove(coords)
-            if len(open_coords) > 0 and args.tag is not None:
-                message = args.tag + "\n"
-            else:
-                message = "Keine "
-            message += f"Freie 8er {timestamp}"
-            for coord in open_coords:
-                message += f"\n- {coord}"
-            if args.webhook:
-                webhook = SyncWebhook.from_url(args.webhook)
-                webhook.send(message)
-            else:
-                print(message)
+        open_coords = []
+        galaxies = []
+        for planet in root.findall('planet'):
+            coords = planet.get("coords")
+            coord_list = coords.split(":")
+            galaxy = coord_list[0]
+            if galaxy not in galaxies:
+                galaxies.append(galaxy)
+                for system in range(1, 500):
+                    for position in range(1, 16):
+                        if (args.galaxy is None or args.galaxy == galaxy) and (args.system is None or args.system == system) and (args.position is None or args.position == position):
+                            open_coords.append(f"{galaxy}:{system}:{position}")
+            if (args.galaxy is None or int(coord_list[-3]) == args.galaxy) and (args.system is None or int(coord_list[-2]) == args.system) and (args.position is None or int(coord_list[-1]) == args.position):
+                open_coords.remove(coords)
+        if len(open_coords) > 0 and args.tag is not None:
+            message = args.tag + "\n"
+        else:
+            message = "Keine "
+        message += f"Freie 8er {timestamp}"
+        for coord in open_coords:
+            message += f"\n- {coord}"
+        if args.webhook:
+            webhook = SyncWebhook.from_url(args.webhook)
+            webhook.send(message)
+        else:
+            print(message)
